5e4726086cad4022288ec774c697b72c6f40cae4
lasha tatulashvili <lashatatu@gmail.com>
2021-03-23 22:03:08 +0400

    64 404

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import {router} from 'next/client';
 
 function ClientProjects (props) {
+
+	function loadProjectHandler(){
+		router.push('/clients/max/projecta')
+	}
+
 	return (
 		 <div>
 			 <h1>client projects</h1>
+			 <button onClick={loadProjectHandler}>load project a</button>
 		 </div>
 	);
 }
