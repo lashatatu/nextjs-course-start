2ab376a94591544e244900423e791d5fd7e7bae5
lasha tatulashvili <lashatatu@gmail.com>
2021-03-23 21:59:30 +0400

    63 programatically

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import Link from 'next/link';
 
 function ClientsPage (props) {
+	const clients = [
+		{
+			id: 'max',
+			name: 'Maximilian'
+		},
+		{
+			id: 'manu',
+			name: 'manuel'
+		},
+		{
+			id: 'lasha',
+			name: 'lashatatu'
+		}
+	];
 	return (
 		 <div>
 			 <h1>clients page</h1>
+			 <ul>
+				 {clients.map(client =>
+						<li key={client.id}>
+							<Link
+								 href={{
+									 pathname: '/clients/[id]',
+									 query: {id: client.id}
+								 }}>{client.name}</Link>
+						</li>)}
+			 </ul>
 		 </div>
 	);
 }
